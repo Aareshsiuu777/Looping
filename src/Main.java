c460cd5cc5fba15f5b06060bd0d64d58cf9569eb
Aareshsiuu777 <maharjah@mail.uc.edu>
2026-02-26 22:41:45 -0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+        int i = 0;
+        while (i <= 24) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("\n");
+
+        int j = 15;
+        while (j >= 0) {
+            System.out.print(j + " ");
+            j--;
+        }
+        System.out.println("\n");
+
+        int k = 0;
+        while (k <= 75) {
+            System.out.print(k + " ");
+            k += 5;
+        }
+        System.out.println("\n");
+
+        int l = 18;
+        while (l >= 0) {
+            System.out.print(l + " ");
+            l -= 2;
+        }
+        System.out.println("\n");
+
+
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int row = 5; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
